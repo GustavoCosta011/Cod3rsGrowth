516b85ae251511fdc371754eb4b58497903e1986
GustavoC <gcostapereira22@gmail.com>
2024-05-14 12:49:40 -0300

    não tinha salvado

--- Cod3rsGrowth.Dominio/Clube.cs
+++ Cod3rsGrowth.Dominio/Clube.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Cod3rsGrowth.Dominio
 {
-    internal class Class2
+    public class Clube
     {
+        public int Id { get; }
+        public string Nome { get; }
+        public DateTime Fundacao { get; set; }
+        public string? Estadio { get; set; }
+        public EstadosEnum Estado { get; set; }
+        public bool CoberturaAntiChuva { get; set; }
+        public List<Jogadores>? Elenco { get; set; }
+
+        
+
+
     }
 }
--- Cod3rsGrowth.Dominio/Jogador.cs
+++ Cod3rsGrowth.Dominio/Jogador.cs
@@ -1,40 +1,16 @@
 ﻿namespace Cod3rsGrowth.Dominio
 {
 
-    public class Jogadores
+    public class Jogador
     {
         public int Id { get; }
-        public string? Nome { get; }
+        public string Nome { get; }
         public int Idade { get; }
         public DateTime DataDeNascimento { get; set; } 
         public float Altura { get; set; }
         public float Peso { get; set; }
 
-        public Jogadores()
-        {
-
-        }
 
     }
-
-
-
-
-    public class Clubes
-    {
-        public int Id { get; }
-        public string? Nome { get; }
-        public DateTime Fundacao { get; set; }
-        public string? Estadio { get; set; }
-        public EstadosEnum Estado { get; set; }
-        public bool CoberturaAntiChuva { get; set; }
-        public List<Jogadores>? Elenco {  get; set; }
-
-        public Clubes()
-        {
-
-        }
-
-
-    }
+    
 }
