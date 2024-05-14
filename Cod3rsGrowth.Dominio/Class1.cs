7260ec892ee498dc6a98ccea98b41bb4171e9f3b
GustavoC <gcostapereira22@gmail.com>
2024-05-14 11:35:13 -0300

    adicionar classes criadas

--- Cod3rsGrowth.Dominio/Class1.cs
+++ Cod3rsGrowth.Dominio/Class1.cs
@@ -1,7 +1,35 @@
 ﻿namespace Cod3rsGrowth.Dominio
 {
-    public class Class1
+
+    public class Jogadores
     {
+        public int Id { get; }
+        public string? Nome { get; }
+        public int Idade { get; }
+        public DateTime DataDeNascimento { get; set; } 
+        public float Altura { get; set; }
+        public float Peso { get; set; }
+
+        public Jogadores()
+        {
+
+        }
+
+    }
+
+
+
+
+    public class Clubes
+    {
+        public int Id { get; }
+        public string? Nome { get; }
+        public DateTime Fundacao { get; set; }
+        public string Estadio { get; set; }
+        public EstadosEnum Estado { get; set; }
+        public bool CoberturaAntiChuva { get; set; }
+        public List<Jogadores>? Elenco {  get; set; }
+
 
     }
 }
