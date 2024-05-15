8e6d1012d5b7f7f42b3628b509a9563525b138af
GustavoC <gcostapereira22@gmail.com>
2024-05-15 10:03:21 -0300

    adicionar modulo de injeção

---
+++ Cod3rsGrowth.Test/TesteBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Cod3rsGrowth.Test
+{
+
+
+    public class Teste : IDisposable
+    {
+        protected ServiceProvider _serviceProvider;
+
+
+        public Teste(ServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
