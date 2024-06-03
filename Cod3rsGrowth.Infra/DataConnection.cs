17d32ddb6de1aeeaea9c2e34786575ded4c83b65
GustavoC <gcostapereira22@gmail.com>
2024-06-03 13:46:02 -0300

    Config do banco de daods

---
+++ Cod3rsGrowth.Infra/DataConnection.cs
@@ -0,0 +1,15 @@
+﻿using LinqToDB;
+using LinqToDB.Configuration;
+using LinqToDB.Data;
+using LinqToDB.SchemaProvider;
+
+namespace Cod3rsGrowth.Infra
+{
+    public class ProjetoDataConnection : DataConnection
+    {
+        public ProjetoDataConnection(DataOptions<ProjetoDataConnection> options) : base(options.Options)
+        {
+
+        }
+    }
+}
