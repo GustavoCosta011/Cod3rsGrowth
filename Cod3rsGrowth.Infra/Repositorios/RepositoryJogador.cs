28c3632bb700a4fdc43e349b613d8a3285759ebb
GustavoC <gcostapereira22@gmail.com>
2024-06-03 10:44:28 -0300

    Config do banco de daods

--- Cod3rsGrowth.Infra/Repositorios/RepositoryJogador.cs
+++ Cod3rsGrowth.Infra/Repositorios/RepositoryJogador.cs
@@ -1,59 +1,33 @@
 ﻿using Cod3rsGrowth.Dominio.Modelos;
 using Cod3rsGrowth.Dominio.Interfaces;
 
-
-
 namespace Cod3rsGrowth.Test.Repositorios;
 
 public class RepositoryJogador : IRepositoryData<Jogador>
 {
-
-    public List<Jogador> ListaJogador;
-    public Jogador? jogador;
-
-
-    public List<Jogador>? ObterTodos()
+    public int Criar(Jogador objeto)
     {
-        return ListaJogador;
+        throw new NotImplementedException();
+    }
+
+    public void Editar(int id, Jogador objeto)
+    {
+        throw new NotImplementedException();
     }
 
     public Jogador ObterPorId(int id)
     {
-        return ListaJogador.Find(jogador => jogador.Id == id) ?? throw new Exception("Jogador inexistente!");
+        throw new NotImplementedException();
     }
 
-    public int Criar(Jogador jogador)
+    public List<Jogador> ObterTodos()
     {
-        int IncremntoCriar = 1;
-        jogador.Id = ListaJogador.Any() ? ListaJogador.Max(jogador => jogador.Id) + IncremntoCriar : IncremntoCriar;
-
-        ListaJogador.Add(jogador);
-
-        return jogador.Id;
-
-    }
-
-    public void Editar(int idDoEdit, Jogador jogador)
-    {
-        var Editado = ObterPorId(idDoEdit);
-       
-            Editado.Nome = jogador.Nome;
-
-            Editado.Idade = jogador.Idade;
-        
-            Editado.DataDeNascimento = jogador.DataDeNascimento;
-
-            Editado.Altura = jogador.Altura;
-
-            Editado.Peso = jogador.Peso;
-
+        throw new NotImplementedException();
     }
 
     public void Remover(int id)
-
     {
-        var jogadorARemover = ObterPorId(id);
-        ListaJogador.Remove(jogadorARemover);
+        throw new NotImplementedException();
     }
 }
 
