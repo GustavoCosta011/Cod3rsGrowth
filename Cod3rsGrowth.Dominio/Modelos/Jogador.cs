a65fc4c9e6090828c6a827d8e82de64ab8ffb379
GustavoC <gcostapereira22@gmail.com>
2024-06-10 11:29:03 -0300

    Criando FluentMigrator

--- Cod3rsGrowth.Dominio/Modelos/Jogador.cs
+++ Cod3rsGrowth.Dominio/Modelos/Jogador.cs
@@ -1,13 +1,21 @@
-﻿namespace Cod3rsGrowth.Dominio.Modelos
-{
+﻿using LinqToDB.Mapping;
 
+namespace Cod3rsGrowth.Dominio.Modelos
+{
+    [Table("Jogador")]
     public class Jogador
     {
+        [PrimaryKey, Identity]
         public int Id { get; set; }
+        [Column("NomeDoJogador"), NotNull]
         public string Nome { get; set; }
+        [Column("Idade")]
         public int? Idade { get; set; }
+        [Column("DataDeNascimento"), NotNull]
         public DateTime DataDeNascimento { get; set; }
+        [Column("Altura")]
         public double? Altura { get; set; }
+        [Column("Peso")]
         public double? Peso { get; set; }
 
         public Jogador(int id, string nome, int? idade, DateTime dataDeNascimento, double? altura, double? peso)
