ba551c6389dbc0faff5bde08297d864742fb42b1
GustavoC <gcostapereira22@gmail.com>
2024-06-25 10:31:22 -0300

    refatorando

--- Cod3rsGrowth.Servicos/ModuloInjetorServico.cs
+++ Cod3rsGrowth.Servicos/ModuloInjetorServico.cs
@@ -1,18 +1,17 @@
-﻿
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Cod3rsGrowth.Servicos.Servicos;
 using Cod3rsGrowth.Servicos.Validadores;
 
-namespace Cod3rsGrowth.Test
+namespace Cod3rsGrowth.Servicos
 {
     public static class ModuloInjetorServico
     {
         public static void Servicos(IServiceCollection ServicosServico)
         {
-            ServicosServico.AddSingleton<ServicoClube>();
-            ServicosServico.AddSingleton<ServicoJogador>();
-            ServicosServico.AddSingleton<ValidadorJogador>();
-            ServicosServico.AddSingleton<ValidadorClube>();
+            ServicosServico.AddScoped<ServicoClube>();
+            ServicosServico.AddScoped<ServicoJogador>();
+            ServicosServico.AddScoped<ValidadorJogador>();
+            ServicosServico.AddScoped<ValidadorClube>();
         }
     }
 }
