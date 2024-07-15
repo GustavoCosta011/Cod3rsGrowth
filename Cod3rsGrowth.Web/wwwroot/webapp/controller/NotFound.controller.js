2669fff16a2b173119e8a9bcc32fae19bf1c5f73
GustavoC <gcostapereira22@gmail.com>
2024-07-15 09:17:47 -0300

    tela de not found  com retorno

--- Cod3rsGrowth.Web/wwwroot/webapp/controller/NotFound.controller.js
+++ Cod3rsGrowth.Web/wwwroot/webapp/controller/NotFound.controller.js
@@ -3,7 +3,19 @@ sap.ui.define([
  ], function (Controller) {
     "use strict";
     return Controller.extend("cod3rsgrowth.controller.NotFound", {
-       onInit: function () {
-       }
+         onInit: function () {
+         },
+
+         onNavBack() {
+            const oHistory = History.getInstance();
+            const sPreviousHash = oHistory.getPreviousHash();
+
+            if (sPreviousHash !== undefined) {
+               window.history.go(-1);
+            } else {
+               const oRouter = this.getOwnerComponent().getRouter();
+               oRouter.navTo("home", {}, true);
+            }
+         }
     });
  });
