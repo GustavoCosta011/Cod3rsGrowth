a1e6a3540f9a89a3a3a439b425ee3c600302cd57
GustavoC <gcostapereira22@gmail.com>
2024-07-29 14:08:34 -0300

    tela de filtro estado e data funcionando

--- Cod3rsGrowth.Web/wwwroot/webapp/test/jornadas/ListaDeClubes.js
+++ Cod3rsGrowth.Web/wwwroot/webapp/test/jornadas/ListaDeClubes.js
@@ -23,14 +23,43 @@ sap.ui.define([
         Then.naPaginaListaDeClubes.buscarSeItemsForamAdicionados();
         
     });
-    opaQUnit("Deve varificar se a lista foi filtrada por nome", function (Given, When, Then) {
-      //Act
-      When.naPaginaListaDeClubes.aoInserirFiltroNome("Flamengo");
-      //Assert
-      Then.naPaginaListaDeClubes.varificarSeFoiRetornadaListaComFiltroNome("Flamengo");
 
-      Then.iTeardownMyApp();
+    opaQUnit("Deve varificar se a lista foi filtrada por nome", function (Given, When, Then) {
+        //Act
+        When.naPaginaListaDeClubes.aoInserirFiltroNome("Flamengo");
+        //Assert
+        Then.naPaginaListaDeClubes.verificarSeFoiRetornadaListaComFiltroNome("Flamengo");
+        Then.iTeardownMyApp();
     });
 
+    opaQUnit("Deve verificar se a lista foi filtrada por estado", function(Given, When, Then){
+        // Arrange
+        Given.iStartMyApp();
+
+        //Parar que possa ser excutado os testes na pagina de lista
+        When.naPaginaHome.aoClicarEmClubes();
+
+        //Act
+        When.naPaginaListaDeClubes.aoInserirFiltroEstado("Rio de Janeiro");
+        //Assert
+        Then.naPaginaListaDeClubes.verificarSeFoiRetornadaListaComFiltroEstado("Rio de Janeiro");
+
+        Then.iTeardownMyApp();      
+    });
+
+    opaQUnit("Deve verificar se a lista foi filtrada por Data", function(Given, When, Then){
+      // Arrange
+      Given.iStartMyApp();
+
+      //Parar que possa ser excutado os testes na pagina de lista
+      When.naPaginaHome.aoClicarEmClubes();
+
+      //Act
+      When.naPaginaListaDeClubes.aoInserirFiltroData("25/03/1924 - 02/01/1931");
+      //Assert
+      Then.naPaginaListaDeClubes.verificarSeFoiRetornadaListaComFiltroFundacao("03/25/1924","01/02/1931");
+
+      Then.iTeardownMyApp();      
+    });
 
 });
--- Cod3rsGrowth.Web/wwwroot/webapp/test/pages/ListadeClubes.js
+++ Cod3rsGrowth.Web/wwwroot/webapp/test/pages/ListadeClubes.js
@@ -30,6 +30,22 @@ sap.ui.define([
                         errorMessage: "Campo de busca para filtrar por nome não encontrado."
                     });
                 },
+                aoInserirFiltroEstado: function(estado){
+                    return this.waitFor({
+                        id: "ComboBoxEstados",
+                        viewName: dataView,
+                        actions: new EnterText({text : estado}),
+                        errorMessage: "Campo de busca ComboBoxEstado não encontrado."
+                    })
+                },
+                aoInserirFiltroData: function(dataRange){
+                     return this.waitFor({
+                        id: "calendario",
+                        viewName: dataView,
+                        actions: new EnterText({text : dataRange}),
+                        errorMessage: "Campo de busca fundação não encontrado."
+                     })
+                }
             },
             assertions: {
                 buscarUrlDaPaginaDeClubes: function() {
@@ -80,7 +96,7 @@ sap.ui.define([
 						errorMessage: "Os dados não foram carregados"
 					});                   
                 },
-                varificarSeFoiRetornadaListaComFiltroNome: function(filtroNome) {
+                verificarSeFoiRetornadaListaComFiltroNome: function(filtroNome) {
                     return this.waitFor({
                         id: "ListaDeClubes",
                         viewName: dataView,
@@ -99,6 +115,58 @@ sap.ui.define([
                         },
                         errorMessage: "A lista não contém o nome: " + filtroNome
                     });
+                },
+                verificarSeFoiRetornadaListaComFiltroEstado: function(estado) {
+                    return this.waitFor({
+                        id: "ListaDeClubes",
+                        viewName: dataView,
+                        success: function(oList) {
+                            var aItems = oList.getItems();
+                            var bFound = aItems.some(function(oItem) {
+                                var oContext = oItem.getBindingContext();
+                                if (oContext) {
+                                    var sEstado = oContext.getProperty("estado");
+                                    return sEstado.includes(estado);
+                                }
+                                return false;
+                            });
+
+                            Opa5.assert.ok(bFound, "A lista contém o Estado: " + estado);
+                        },
+                        errorMessage: "A lista não contém o Estado: " + estado
+                    });
+                },
+                verificarSeFoiRetornadaListaComFiltroFundacao: function(DataPiso, DataTeto) {
+                    return this.waitFor({
+                        id: "ListaDeClubes",
+                        viewName: dataView,
+                        success: function(oList) {
+                            var aItems = oList.getItems();
+                            var bFound = aItems.some(function(oItem) {
+                                var oContext = oItem.getBindingContext();
+                                if (oContext) {
+                                    var sData = oContext.getProperty("fundacao");
+                                    if (sData) {
+                                        var dataFundacao = new Date(sData);
+                                        var dataPiso = new Date(DataPiso);
+                                        var dataTeto = new Date(DataTeto);
+                
+                                        console.log("Data Fundação: ", dataFundacao);
+                                        console.log("Data Piso: ", dataPiso);
+                                        console.log("Data Teto: ", dataTeto);
+                
+                                        return dataFundacao >= dataPiso && dataFundacao <= dataTeto;
+                                    }
+                                    console.log("Data da Fundação não encontrada");
+                                    return false;
+                                }
+                                console.log("Contexto não encontrado");
+                                return false;
+                            });
+                            Opa5.assert.ok(bFound, "A lista contém fundações entre: " + DataPiso + " e " + DataTeto);
+                        },
+                        errorMessage: "A lista não contém fundações entre: " + DataPiso + " e " + DataTeto
+                    });                
                 }
             }
         }
